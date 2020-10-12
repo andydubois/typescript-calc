df603f78d634d83b4867bc92f75dbef88ac2400e
Andy DuBois <andydubois89@gmail.com>
2020-10-12 01:20:18 -0500

    added comments

--- server/routes/math.router.js
+++ server/routes/math.router.js
@@ -3,31 +3,34 @@ const pool = require("../modules/pool");
 require("dotenv").config();
 const router = express.Router();
 
-
 //adds equations entered on the DOM to the database
 router.post("/add", (req, res) => {
-    const queryText = `INSERT INTO equations (equation, result) VALUES ($1, $2)`;
-    pool.query(queryText, [req.body.input, req.body.result])
-    .then(result => {
-        console.log("added new equation to the database", req.body);
-        res.sendStatus(201);
+  //turns SQL statement into a variable
+  const queryText = `INSERT INTO equations (equation, result) VALUES ($1, $2)`;
+  pool
+    .query(queryText, [req.body.input, req.body.result])
+    .then((result) => {
+      console.log("added new equation to the database", req.body);
+      res.sendStatus(201);
     })
-    .catch(error => {
-        console.log("error adding equation to database", error);
-        res.sendStatus(500);
-    })
-})
+    .catch((error) => {
+      console.log("error adding equation to database", error);
+      res.sendStatus(500);
+    });
+});
 
+//retrieves most recent 10 equations from database
 router.get("/equations", (req, res) => {
-    const queryText = `SELECT * FROM equations ORDER BY id DESC LIMIT 10`;
-    pool.query(queryText)
-    .then(result => {
-        res.send(result.rows);
+  const queryText = `SELECT * FROM equations ORDER BY id DESC LIMIT 10`;
+  pool
+    .query(queryText)
+    .then((result) => {
+      res.send(result.rows);
     })
-    .catch(error => {
-        console.log("error in server side equations GET", error);
-        res.sendStatus(500);
-    })
-})
+    .catch((error) => {
+      console.log("error in server side equations GET", error);
+      res.sendStatus(500);
+    });
+});
 
 module.exports = router;
